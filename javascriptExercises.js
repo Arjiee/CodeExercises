851d0dde88d26f5fd1fc4b10e5c76b97e7063513
Arjiee <acostaarjay17.aa@gmail.com>
2026-05-05 16:51:12 +0800

    plits the string into strings of two characters

--- javascriptExercises.js
+++ javascriptExercises.js
@@ -98,3 +98,27 @@ function duplicateCount(text){
   }
   return count;
 }
+
+// Complete the solution so that it splits the string into strings of two characters 
+// in a list/array (depending on the language you use). If the string contains an 
+// odd number of characters then it should replace the missing second character of the 
+// final pair with an underscore ('_').
+
+// Examples:
+
+// * 'abc' =>  ['ab', 'c_']
+// * 'abcdef' => ['ab', 'cd', 'ef']
+
+function solution(str){
+   let result = [];
+  
+  if (str.length % 2 !== 0){
+    str += "_";
+  }
+  
+  for (let i = 0; i < str.length; i += 2){
+    result.push(str.substring(i, i + 2));
+  }
+  
+  return result;
+}
